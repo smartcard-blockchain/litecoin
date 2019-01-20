2f9fc7bc59e825f5f94ae27cd076f0ad116cab5d
ubuntu <mr.shahbaz.aslam+github-smartcard-blockchain@gmail.com>
2019-01-20 14:45:03 +0400

    update 'magic number' in pchMessageStart to unique value

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,10 +113,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0x43;
+        pchMessageStart[1] = 0x41;
+        pchMessageStart[2] = 0x52;
+        pchMessageStart[3] = 0x44;
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
 
